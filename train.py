f8018447f740921c67b3aff7aaf596ae77ba1315
Soohwan Park <shpark1359@gmail.com>
2020-03-26 16:33:13 +0900

    change damping

--- train.py
+++ train.py
@@ -3,9 +3,10 @@ from ppo import TrackingController
 
 tc = TrackingController()
 tc.initialize(
-    session_name="test2",
+    session_name="test3",
     num_slaves=8,
 )
+tc.loadNetworks(directory="./output/test2", network_type=None)
 
 for i in range(100000):
     tc.runTraining(1)
